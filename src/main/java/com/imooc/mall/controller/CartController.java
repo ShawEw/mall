a38a13ca6b11cb1edb2e830acb6513c4569fd3f6
PC-20200301LBDZ\Administrator <729845809@qq.com>
2020-12-02 21:47:23 +0800

    新增接口

--- src/main/java/com/imooc/mall/controller/CartController.java
+++ src/main/java/com/imooc/mall/controller/CartController.java
@@ -1,17 +1,20 @@
 package com.imooc.mall.controller;
 
+import com.imooc.mall.consts.MallConst;
 import com.imooc.mall.dao.ProductMapper;
 import com.imooc.mall.enums.ProductStatus;
 import com.imooc.mall.enums.ResponseEnum;
 import com.imooc.mall.form.CartAddForm;
+import com.imooc.mall.form.CartUpdateForm;
 import com.imooc.mall.pojo.Product;
+import com.imooc.mall.pojo.User;
+import com.imooc.mall.service.ICartService;
 import com.imooc.mall.vo.CartVo;
 import com.imooc.mall.vo.ResponseVo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 import java.text.Normalizer;
 
@@ -24,21 +27,48 @@ import java.text.Normalizer;
 public class CartController {
 
     @Autowired
-    private ProductMapper productMapper;
+    private ICartService cartService;
+
+    @GetMapping("/carts")
+    public ResponseVo<CartVo> list(HttpSession session){
+        User user = (User) session.getAttribute(MallConst.CURRENT_USER);
+        return cartService.list(user.getId());
+    }
 
     @PostMapping("/carts")
-    public ResponseVo<CartVo> add(@Valid @RequestBody CartAddForm cartAddForm){
-        Product product = productMapper.selectByPrimaryKey(cartAddForm.getProductId());
-        if (product == null){
-            return ResponseVo.error(ResponseEnum.PRODUCT_NOT_EXIST);
-        }
-        if (!product.getStatus().equals(ProductStatus.ON_SALE.getCode())){
-            return ResponseVo.error(ResponseEnum.PRODUCT_OFF_SALE_OR_DELETE);
-        }
-        if (product.getStock() <= 0){
-            return ResponseVo.error(ResponseEnum.PRODUCT_STOCK_ERROR);
-        }
-        
-        return null;
+    public ResponseVo<CartVo> add(@Valid @RequestBody CartAddForm cartAddForm, HttpSession session){
+        User user = (User) session.getAttribute(MallConst.CURRENT_USER);
+        return cartService.add(user.getId(), cartAddForm);
     }
+
+    @PutMapping("/carts/{productId}")
+    public ResponseVo<CartVo> update(@PathVariable Integer productId, @Valid @RequestBody CartUpdateForm form, HttpSession session){
+        User user = (User) session.getAttribute(MallConst.CURRENT_USER);
+        return cartService.update(user.getId(), productId, form);
+    }
+
+    @DeleteMapping("/carts/{productId}")
+    public ResponseVo<CartVo> delete(@PathVariable Integer productId, HttpSession session){
+        User user = (User) session.getAttribute(MallConst.CURRENT_USER);
+        return cartService.delete(user.getId(), productId);
+    }
+
+    @PutMapping("/carts/selectAll")
+    public ResponseVo<CartVo> selectAll(HttpSession session){
+        User user = (User) session.getAttribute(MallConst.CURRENT_USER);
+        return cartService.selectAll(user.getId());
+    }
+
+    @PutMapping("/carts/unSelectAll")
+    public ResponseVo<CartVo> unSelectAll(HttpSession session){
+        User user = (User) session.getAttribute(MallConst.CURRENT_USER);
+        return cartService.unSelectAll(user.getId());
+    }
+
+    @GetMapping("/carts/products/sum")
+    public ResponseVo<Integer> sum(HttpSession session){
+        User user = (User) session.getAttribute(MallConst.CURRENT_USER);
+        return cartService.sum(user.getId());
+    }
+
 }
